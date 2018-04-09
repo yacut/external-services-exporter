e43683186c266c06acf8d0cb25a2d8bcee5dd371
yacut <roman.rogozhnikov@gmail.com>
2018-04-09 12:22:18 +0200

    remove old code

--- src/index.ts
+++ src/index.ts
@@ -60,31 +60,6 @@ app.get("/healthz", (req: any, res: any) => {
 
 app.get("/metrics", async (req: any, res: any) => {
     try {
-        const namespaces = await client.api.v1.namespaces.get();
-        if (namespaces.statusCode === 200){
-            namespaces.body.items.forEach(async (item: any) => {
-                const namespace = item.metadata.name;
-                const services = await client.api.v1.namespaces(namespace).services.get();
-                if (services.statusCode === 200) {
-                    services.body.items.forEach((service: any) => {
-                        const name = service.metadata.name;
-                        const type = service.spec.type;
-                        const external_ip = service.status.loadBalancer.ingress ? service.status.loadBalancer.ingress[0] : ""
-                        const ip_filter = service.spec.loadBalancerSourceRanges ? service.spec.loadBalancerSourceRanges.join(",") : "";
-                        gauge.labels(name, namespace, external_ip, type, ip_filter).set(1);
-                    });
-                }
-                const ingresses = await client.apis.extensions.v1beta1.namespaces(namespace).ingresses.get();
-                if (ingresses.statusCode === 200) {
-                    ingresses.body.items.forEach((ingress: any) => {
-                        const name = ingress.metadata.name;
-                        const external_ip = ingress.status.loadBalancer.ingress ? ingress.status.loadBalancer.ingress[0] : ""
-                        const ip_filter = ingress.annotations ? ingress.annotations["ingress.kubernetes.io/whitelist-source-range"] : "";
-                        gauge.labels(name, namespace, external_ip, "ingress", ip_filter).set(1);
-                    });
-                }
-            });
-        }
         res.set("Content-Type", register.contentType)
         res.end(register.metrics())
     } catch(e) {
